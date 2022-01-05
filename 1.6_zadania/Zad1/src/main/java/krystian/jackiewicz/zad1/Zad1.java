a72f67a46b16bace22efc93788bb66a1315c173d
KrystianJc <jackiewicz.krystian4@gmail.com>
2022-01-05 08:44:13 +0100

    Szkielet

---
+++ 1.6_zadania/Zad1/src/main/java/krystian/jackiewicz/zad1/Zad1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package krystian.jackiewicz.zad1;
+
+/**
+ *
+ * @author jacki
+ */
+public class Zad1 {
+    public static void main(String args[])
+    {
+        
+    }
+}

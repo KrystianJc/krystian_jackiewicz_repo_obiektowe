eaab785af61e92e1379039bf886509c70161f16e
KrystianJc <jackiewicz.krystian4@gmail.com>
2022-02-02 09:24:36 +0100

    Przeslanie pliku

--- SortowanieTablicy/SortowanieTablic.java
+++ SortowanieTablicy/SortowanieTablic.java
@@ -1,20 +1,26 @@
 import java.util.Random;
-
+import java.util.Arrays;
 public class SortowanieTablic {
     public static void main(String[] args) {
-        final int ilosc_Elementow = 100000;
+        final int ilosc_Elementow = 50000;
 
         Random r = new Random();
-        int[] tab = new int[ilosc_Elementow];
-
-        for (int i = 0; i < 100; i++) {
-            tab[i] = r.nextInt(1000000) + 1;
-        }
-
-
-        long millisActualTime = System.currentTimeMillis();
-        // Sortowanie bąbelkowe
         
+        long millisActualTime = 0, executionTime = 0;
+        for(int k=0;k<10;k++)
+        {
+            int[] tabBubble = new int[ilosc_Elementow];
+            for (int i = 0; i < ilosc_Elementow; i++) {
+                tabBubble[i] = r.nextInt(1000000) + 1;
+            }
+        int[] tabQuick = Arrays.copyOf(tabBubble,tabBubble.length);
+        millisActualTime = System.currentTimeMillis();
+        Arrays.sort(tabQuick);
+
+        executionTime = System.currentTimeMillis() - millisActualTime;
+        System.out.println("Sortowanie QUICKSORT czas wykonania: "+executionTime);
+        millisActualTime = System.currentTimeMillis();
+        //Sortowanie babelkowe
         for (int i = 0; i < tab.length; i++) {
             for (int j = 1; j < tab.length; j++) {
                 if (tab[j] < tab[j - 1]) {
@@ -24,9 +30,11 @@ public class SortowanieTablic {
                 }
             }
         }
-        long executionTime = System.currentTimeMillis() - millisActualTime; 
-        System.out.println("Sortowanie babelkowe czas wykonania: "+executionTime);
 
+      executionTime = System.currentTimeMillis() - millisActualTime;
+        
+        System.out.println("Sortowanie babelkowe czas wykonania: "+executionTime);
+    }
 
     }
 
